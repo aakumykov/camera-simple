69780b4fbcd2b362f907f8751ad996d7194cc975
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-27 11:44:37 +0700

    Простая инициализация через <script>.

--- www/js/index.js
+++ www/js/index.js
@@ -1,55 +1,41 @@
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-        document.getElementById('photo_button').addEventListener('click', function(){
-            console.log('photo_button click...');
+//"use strict";
 
-            takeAPhoto();
-        });
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        console.log('device is ready');
-        console.log('camera: '+navigator.camera);
-    },
-};
+var app = angular.module("CameraApp",[]);
 
-app.initialize();
+app.controller("CameraController", function($scope){
+	$scope.title = "Photocamera";
+	$scope.photo_src = '';
+	$scope.takeAPhoto = function(){
+		console.log('takeAPhoto()');
 
-function takeAPhoto(){
-    console.log('takeAPhoto()');
+		navigator.camera.getPicture(
+			$scope.onSuccess, 
+			$scope.onFail, 
+			{ 
+				quality: 50,
+				encodingType: Camera.EncodingType.JPEG,
+				sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+				//sourceType: Camera.PictureSourceType.CAMERA,
+				destinationType: Camera.DestinationType.FILE_URI,
+			}
+		);
+	}
+    $scope.onSuccess = function(imageURI) {
+        console.info('onSuccess(): SUCCESS');
+        
+        var image = document.getElementById('photo');
+            image.src = "data:image/jpeg;base64," + imageURI;
+    }
+    $scope.onFail = function(message) {
+        console.error('onFail(): '+message);
+    }
+});
 
-    navigator.camera.getPicture(
-        onSuccess, 
-        onFail, 
-        { 
-            quality: 50,
-            encodingType: Camera.EncodingType.JPEG,
-            sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
-            //sourceType: Camera.PictureSourceType.CAMERA,
-            destinationType: Camera.DestinationType.FILE_URI,
-        }
-    );
-}
 
-function onSuccess(imageURI) {
-    console.info('onSuccess(): SUCCESS');
-    
-    var image = document.getElementById('photo');
-        image.src = "data:image/jpeg;base64," + imageURI;
-}
-
-function onFail(message) {
-    console.error('onFail(): '+message);
-}
+// document.addEventListener(
+//     'deviceready',
+//     function(){
+//         angular.bootstrap(document, ['CameraApp']);
+//     },
+//     false
+// );
